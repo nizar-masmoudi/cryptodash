cfb479e72377eb3dfe231dc5073b084d195e7c81
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-04-24 19:27:04 +0100

    Minor style fix

--- components/sidebar.py
+++ components/sidebar.py
@@ -4,4 +4,4 @@ class SidebarAIO(html.Div):
   def __init__(self):
     super().__init__([
       html.H1('CryptoDash', className = 'text-4xl font-semibold text-center mb-16')
-    ], className = 'fixed inset-0 bg-[#13151B] w-1/5 h-screen text-white/75 font-light py-10')
+    ], className = 'fixed inset-0 bg-[#13151B] w-1/5 h-screen text-white font-light py-10')
