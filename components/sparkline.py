48f48d5c139b8da17b5704711d55c11e21710156
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-04-25 12:34:54 +0100

    Add period dropdown

--- components/sparkline.py
+++ components/sparkline.py
@@ -1,10 +1,12 @@
-from dash import html, dcc
+from dash import html, dcc, callback, Input, Output, MATCH
 import plotly.graph_objects as go
 import uuid
 from api import APIClient
 from dotenv import load_dotenv
 import os
 from math import floor, ceil
+import re
+from datetime import datetime, timedelta
 
 load_dotenv() # Dev only
 api = APIClient(api_key = os.getenv('RAPID_API_KEY'), api_host = os.getenv('RAPID_API_HOST'))
@@ -63,7 +65,16 @@ class Sparkline(go.Figure):
 
 class SparklineAIO(html.Span):
   class ids:
-    pass
+    period = lambda aio_id: {
+      'component': 'SparklineAIO',
+      'subcomponent': 'period',
+      'aio_id': aio_id
+    }
+    graph = lambda aio_id: {
+      'component': 'SparklineAIO',
+      'subcomponent': 'graph',
+      'aio_id': aio_id
+    }
   ids = ids
   
   def __init__(self, coin_id = 'Qwsogvtv82FCd', aio_id = None):
@@ -89,12 +100,56 @@ class SparklineAIO(html.Span):
             html.H3('${:,.2f}'.format(float(coin['price'])), className = 'text-2xl font-semibold'),
             html.Div([
               html.Div([], className = 'w-0 h-0 border-l-[6px] border-l-transparent border-b-[10px] border-r-[6px] border-r-transparent -translate-y-px border-b-white {}'.format('rotate-0' if float(coin['change']) > 0 else 'rotate-180')),
-              html.P(coin['change'] + '%'),
-            ], className = 'flex items-center justify-around w-36 h-8 rounded-md p-2 {}'.format('bg-[#26c363]' if float(coin['change']) > 0 else 'bg-red-600'))
+              html.P('{}%'.format(abs(float(coin['change'])))),
+            ], className = 'flex items-center justify-around w-20 h-8 rounded-md p-2 {}'.format('bg-[#26c363]' if float(coin['change']) > 0 else 'bg-red-600'))
           ], className = 'flex items-center space-x-4'),
         ], className = 'flex flex-col justify-between w-56 h-16'),
+        html.Span([
+          html.Span([
+            html.P('Period', className = 'text-lg'),
+            dcc.Dropdown(
+              id = self.ids.period(aio_id),
+              options = ['1 hour' ,'3 hours' ,'12 hours' ,'24 hours' ,'7 days' ,'30 days' ,'3 months' ,'1 year' ,'3 years' ,'5 years'], 
+              value = '24 hours', 
+              clearable = False, 
+              searchable = False,
+            ),
+          ], className = 'flex items-center space-x-4'),
+        ], className = 'flex flex-col justify-center items-end w-full h-16')
       ], className = 'flex items-center space-x-4 h-14'),
       html.Div([
-        dcc.Graph(figure = Sparkline(x = datetimes, y = sparkline, layout = DashLayout(), layout_yaxis_range = [floor(min(sparkline) / 100)*100, ceil(max(sparkline) / 100)*100]))
+        dcc.Graph(
+          id = self.ids.graph(aio_id), 
+          figure = Sparkline(x = datetimes, y = sparkline, layout = DashLayout(), layout_yaxis_range = [floor(min(sparkline) / 100)*100, ceil(max(sparkline) / 100)*100])
+        )
       ], className = 'bg-[#171821] rounded-lg p-6 mt-12 border-[1px] border-white/20'),
     ], className = 'w-full h-full')
+  
+  @callback(
+    Output(ids.graph(MATCH), 'figure'),
+    Input(ids.period(MATCH), 'value'),
+    prevent_initial_call = True,
+  )
+  def update_graph(period):
+    today = datetime.today()
+    
+    period = re.sub(r'(\d{1,2})\s([a-z]{1})[a-z]*', r'\1\2', period)
+    coin = api.get_coin('Qwsogvtv82FCd', period = period)
+    sparkline = coin['sparkline']
+    sparkline = [float(price) for price in sparkline]
+    
+    # datetimes = []
+    # today = datetime.today() - timedelta(hours = 1)
+    # print(d.strftime('%H:%M %p'))
+
+    
+    if period in ['1h', '3h']:
+      datetimes = list(reversed([f'{(t+1)*5}min' for t in range(len(sparkline))]))
+    elif period in ['12h', '24h']:
+      datetimes = list(reversed([f'{t+1}h' for t in range(len(sparkline))]))
+    elif period in ['30d']:
+      datetimes = list(reversed([f'{t+1}d' for t in range(len(sparkline))]))
+    else:
+      datetimes = list(reversed([f'{t+1}' for t in range(len(sparkline))]))
+    
+    return Sparkline(x = datetimes, y = sparkline, layout = DashLayout(), layout_yaxis_range = [floor(min(sparkline) / 100)*100, ceil(max(sparkline) / 100)*100])
