4b4f88fb5a6284b82832b1a1d2aa879cc082a0a8
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-04-28 17:31:49 +0100

    Setup nav links

--- components/input.py
+++ components/input.py
@@ -6,6 +6,7 @@ import uuid
 from dotenv import load_dotenv
 from api import APIClient
 import os
+import dash
 
 load_dotenv() # Dev only
 api = APIClient(api_key = os.getenv('RAPID_API_KEY'), api_host = os.getenv('RAPID_API_HOST'))
@@ -40,7 +41,7 @@ class AutoCompleteAIO(html.Div):
         ], className = 'w-6 h-6 stroke-white/75 fill-none'),
         dcc.Input(className = 'w-full bg-transparent font-light', placeholder = placeholder, id = self.ids.input(aio_id))
       ], className = 'absolute top-0 flex items-center space-x-4 h-12 w-full'),
-      html.Ul(id = self.ids.ul(aio_id), children = [], className = 'absolute top-12 my-2 ml-4')
+      html.Ul(id = self.ids.ul(aio_id), children = [], className = 'absolute top-12 my-2 ml-4 w-full')
     ], className = 'transition-all duration-300 z-20 relative bg-[#21222D] rounded-lg px-4 h-12 min-h-12 w-1/3 overflow-hidden text-white/75 focus-within:border border-[#2595fd]')
   
   @callback(
@@ -49,23 +50,15 @@ class AutoCompleteAIO(html.Div):
     Output(ids.parent(MATCH), 'className'),
     Input(ids.input(MATCH), 'value'),
     Input(ids.parent(MATCH), 'className'),
-    Input({'type': 'option', 'index': ALL}, 'n_clicks'),
-    State({'type': 'option', 'index': ALL}, 'children'),
     prevent_initial_call = True
   )
-  def update_suggestions(value, className, n_clicks, options):
-    # This section covers selection
-    if n_clicks:
-      for i, click in enumerate(n_clicks):
-        if click is not None:
-          return [], options[i], 'transition-all duration-300 z-20 relative bg-[#21222D] rounded-lg px-4 h-12 min-h-12 w-1/3 overflow-hidden text-white/75 focus-within:border border-[#2595fd]' # Default style
-    # This section covers on input value change
+  def update_suggestions(value, className):
     if value:
       coins, _ = api.get_coins(search = value) # limit = 5000
       children = []
       for i, coin in enumerate(coins):
         if value.lower() in coin['name'].lower():
-          children.append(html.Li(id = {'type': 'option', 'index': f'{i}'}, value = coin['uuid'], children = coin['name'], className = 'h-9 cursor-pointer'))
+          children.append(html.Li(id = {'type': 'option', 'index': f'{i}'}, value = coin['uuid'], children = dcc.Link(coin['name'], href = '/coin/' + coin['uuid']), className = 'block w-full h-9 cursor-pointer'))
       height = 48 + 36*min(len(children), 5) + 10 if children else 48
       className = re.sub('h-(\d{2,3}|\[\d{2,3}px\]) min-h-(\d{2,3}|\[\d{2,3}px\])', 'h-[{}px] min-h-[{}px]'.format(height, height), className)
       return children, value, className
--- components/sidebar.py
+++ components/sidebar.py
@@ -1,7 +1,7 @@
-from dash import html
+from dash import html, dcc
 
 class SidebarAIO(html.Div):
   def __init__(self):
     super().__init__([
-      html.H1('CryptoDash', className = 'text-4xl font-semibold text-center mb-16')
+      dcc.Link('CryptoDash', className = 'block text-4xl font-semibold text-center mb-16', href = '/')
     ], className = 'fixed inset-0 bg-[#13151B] w-1/5 h-screen text-white font-light py-10')
